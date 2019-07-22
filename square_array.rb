da1079a1f80f2c119459250826d3cb57a495c9ad
Kyler Barber <kylerbarber820@yahoo.com>
2019-07-22 00:14:05 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+numbers = [1, 2, 3]
+counter = 1
+numbers.each do |candy|
+  puts "this is my loops #{counter}"
+  puts "stop it #{numbers}"
+  counter += 1
 end
